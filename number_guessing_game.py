b69681a4e4e0f75ff53b8a616932124f4eb43592
Tasin-Mahamud <tasinmahamud1234@gmail.com>
2022-08-15 08:55:56 +0600

    Initial commit

---
+++ number_guessing_game.py
@@ -0,0 +1,28 @@
+import random
+print("Welcome to the number guessing game!")
+print("I am thinking of a number between 1 to 100")
+random_number = random.randint(1,100) # device is taking a random number
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard' : \t")
+
+lives = 5
+if (difficulty == "hard"):
+    print(f"you have {lives} attempts remaining to guess the number \n")
+else:
+    print(f"you have {lives+5} attempts remaining to guess the number \n")
+    lives = 10 # if the difficulty is easy, user will get 10 attempts to guess the number
+for i in range(0, lives):
+    user_guess = int(input("Make a guess : \t"))
+
+    if(user_guess < random_number):
+        print("TOO LOW")
+        lives -= 1 # decrease lives by 1
+        print(f"you have {lives} attempts remaining to guess the number \n")
+    elif (user_guess > random_number):
+        print("TOO HIGH")
+        lives -= 1
+        print(f"you have {lives} attempts remaining to guess the number \n")
+    elif (user_guess == random_number):
+        print("YOU'VE WON!!!")
+        break # get out of the loop
+
+print("GAME OVER")
