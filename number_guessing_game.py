fb9189870215c2166402f37266549dc2a046a770
Tasin-Mahamud <tasinmahamud1234@gmail.com>
2022-08-15 08:58:47 +0600

    Initial commit

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,4 +1,16 @@
 import random
+
+print("""
+
+ ,adPPYb,d8 88       88  ,adPPYba, ,adPPYba, ,adPPYba,  
+a8"    `Y88 88       88 a8P_____88 I8[    "" I8[    ""  
+8b       88 88       88 8PP"""""""  `"Y8ba,   `"Y8ba,   
+"8a,   ,d88 "8a,   ,a88 "8b,   ,aa aa    ]8I aa    ]8I  
+ `"YbbdP"Y8  `"YbbdP'Y8  `"Ybbd8"' `"YbbdP"' `"YbbdP"'  
+ aa,    ,88                                             
+  "Y8bbdP" 
+
+""")
 print("Welcome to the number guessing game!")
 print("I am thinking of a number between 1 to 100")
 random_number = random.randint(1,100) # device is taking a random number
